f924c252f99bc91c71aedfbf3b4640a0e0448953
MicheleGravagnone <michelegravagnone@gmail.com>
2026-03-26 22:12:31 +0100

    First commit

---
+++ src/main/java/dsa/exercises/Solution.java
@@ -0,0 +1,42 @@
+package dsa.exercises;
+
+import java.util.*;
+
+public class Solution {
+
+    public int secondLargestElement(int[] arr) {
+        int largest = -1;
+        int secondLargest = -1;
+
+        for (int num : arr) {
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num != largest) {
+                secondLargest = num;
+            }
+        }
+        return secondLargest;
+    }
+
+    public int thirdLargestElement(int[] arr) {
+        int largest = -1;
+        int secondLargest = -1;
+        int thirdLargest = -1;
+
+        for (int num : arr) {
+            if (num > largest) {
+                thirdLargest = secondLargest;
+                secondLargest = largest;
+                largest = num;
+            } else if (num > secondLargest && num != largest) {
+                secondLargest = num;
+            } else if (num > thirdLargest && num != secondLargest) {
+                thirdLargest = num;
+            }
+        }
+
+        return thirdLargest;
+    }
+
+}
